9781d920709701e0bfa9ab9021b0c006c9e34612
Emanoel Justino <emanoelvjj>
2021-12-23 15:46:20 -0300

    Added librarie that stores values ​​securely and implemented method to get movies of service

--- app/src/main/java/br/com/stant/mobile/challenge/data/api/APIResource.kt
+++ app/src/main/java/br/com/stant/mobile/challenge/data/api/APIResource.kt
@@ -2,9 +2,10 @@ package br.com.stant.mobile.challenge.data.api
 
 import br.com.stant.mobile.challenge.domain.model.Movie
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface APIResource {
 
     @GET("/movie/popular")
-    suspend fun getMovies(): BaseResponseAPI<List<Movie>>
+    suspend fun getMovies(@Query("api_key") apiToken: String): BaseResponseAPI<List<Movie>>
 }
