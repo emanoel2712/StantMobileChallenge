6d3db1d241473f96bf8557584e629cb7cf1f81f9
Emanoel Justino <emanoelvjj>
2021-12-23 16:30:10 -0300

    Create some packages and moved some class's

--- app/src/main/java/br/com/stant/mobile/challenge/presenter/view/activity/MainActivity.kt
+++ app/src/main/java/br/com/stant/mobile/challenge/presenter/view/activity/MainActivity.kt
@@ -1,4 +1,4 @@
-package br.com.stant.mobile.challenge.presenter.view
+package br.com.stant.mobile.challenge.presenter.view.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
