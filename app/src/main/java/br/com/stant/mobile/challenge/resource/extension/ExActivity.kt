b11621c264fdbe5fa417ee1901d11325b676bc61
Emanoel Justino <emanoelvjj>
2021-12-25 00:26:15 -0300

    adjsts

--- app/src/main/java/br/com/stant/mobile/challenge/resource/extension/ExActivity.kt
+++ app/src/main/java/br/com/stant/mobile/challenge/resource/extension/ExActivity.kt
@@ -6,12 +6,11 @@ import androidx.appcompat.widget.Toolbar
 import br.com.stant.mobile.challenge.R
 
 fun Activity.hideToolbar() {
-
-    val actionBar = this.findViewById<Toolbar>(R.id.toolbar_main)
-    actionBar.visibility = View.GONE
+    val toolbar = this.findViewById<Toolbar>(R.id.toolbar_main)
+    toolbar.visibility = View.GONE
 }
 
 fun Activity.showToolbar() {
-    val actionBar = this.findViewById<Toolbar>(R.id.toolbar_main)
-    actionBar.visibility = View.VISIBLE
+    val toolbar = this.findViewById<Toolbar>(R.id.toolbar_main)
+    toolbar.visibility = View.VISIBLE
 }
