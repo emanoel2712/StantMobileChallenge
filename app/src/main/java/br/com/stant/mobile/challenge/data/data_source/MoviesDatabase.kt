b1d2fcb5d1703437c7e0dc3a7d5cde3d76c8a7b8
Emanoel Justino <emanoelvjj>
2021-12-25 22:07:27 -0300

    Added room librarie, create singleton class to create movie database

---
+++ app/src/main/java/br/com/stant/mobile/challenge/data/data_source/MoviesDatabase.kt
@@ -0,0 +1,33 @@
+package br.com.stant.mobile.challenge.data.data_source
+
+import android.content.Context
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import br.com.stant.mobile.challenge.resource.utils.Constants
+
+abstract class MoviesDatabase : RoomDatabase() {
+
+    companion object {
+
+        @Volatile
+        private var INSTANCE: MoviesDatabase? = null
+
+        fun getDatabase(context: Context): MoviesDatabase {
+            val tempInstance = INSTANCE
+
+            tempInstance?.let {
+                return it
+            }
+
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    MoviesDatabase::class.java,
+                    Constants.DATABASE_NAME
+                ).fallbackToDestructiveMigration().build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
+}
--- app/src/main/java/br/com/stant/mobile/challenge/resource/utils/Constants.kt
+++ app/src/main/java/br/com/stant/mobile/challenge/resource/utils/Constants.kt
@@ -3,6 +3,6 @@ package br.com.stant.mobile.challenge.resource.utils
 object Constants {
 
     const val DETAIL_MOVIE: String = "DETAIL_MOVIE"
-
+    const val DATABASE_NAME: String = "movies-db"
 
 }
